b7d653778216afe93d2726cc3d92f01074dd8068
BernhardBaumrock <office@baumrock.com>
2023-09-26 14:14:52 +0200

    feat: update module to compile non-core files

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Scss
 
-Module to compile CSS from SCSS
+Module to compile CSS from SCSS, using https://github.com/scssphp/scssphp
+
+## Auto-compile CSS from SCSS when a file changed
+
+You can put this code into `/site/ready.php` to automatically recompile an input file (SCSS) into an output file (CSS) whenever one of the watched files changed:
+
+```php
+/** @var Scss $scss */
+$scss = $modules->get('Scss');
+
+// Find all files with .scss extension in /site/templates/scss.
+// You can adjust that to your needs! Note that this might impact your
+// websites performance. Usually $files->find() is very fast, but the more files
+// you scan, the slower it will get - so try to be specific with your find().
+// Also see https://github.com/processwire/processwire-issues/issues/1791
+$watchFiles = $files->find(
+  $config->paths->templates . "scss",
+  ['extensions' => 'scss']
+);
+
+// compile input SCSS to output CSS
+$scss->compileIfChanged(
+  input: $config->paths->templates . "scss/example.scss",
+  watch: $watchFiles,
+
+  // you can optionally define a custom output path
+  // if not defined it will use the input path and replace .scss with .css
+  // output: '/foo/bar/baz.css',
+
+  // by default the output will be minified, use 'compressed' or 'expanded'
+  // style: 'compressed',
+);
+```
 
 ## Watch core files
 
--- Scss.module.php
+++ Scss.module.php
@@ -3,7 +3,6 @@
 namespace ProcessWire;
 
 use ScssPhp\ScssPhp\Compiler;
-use ScssPhp\ScssPhp\Formatter;
 use ScssPhp\ScssPhp\OutputStyle;
 
 /**
@@ -14,13 +13,13 @@ use ScssPhp\ScssPhp\OutputStyle;
 class Scss extends WireData implements Module
 {
   /** @var Compiler */
-  public $compiler;
+  private $compiler;
 
   public static function getModuleInfo()
   {
     return [
       'title' => 'Scss',
-      'version' => '1.0.1',
+      'version' => '1.1.0',
       'summary' => 'Module to compile CSS files from SCSS',
       'autoload' => false,
       'singular' => true,
@@ -28,10 +27,75 @@ class Scss extends WireData implements Module
     ];
   }
 
-  public function init()
+  /**
+   * Compile input file to output file
+   *
+   * $output can be left empty, then it will take the input file and replace
+   * the .scss ending with .css ending!
+   *
+   * $style can either be 'compressed' or 'expanded'
+   */
+  public function compile(
+    string $input,
+    string $output = null,
+    string $style = 'compressed',
+  ): void {
+    // auto-create output path if no custom one is set
+    if (!$output) $output = substr($input, 0, -4) . "css";
+
+    // get scss content of input file
+    $content = $this->wire->files->fileGetContents($input);
+
+    // get compiler and set output style
+    $compiler = $this->compiler();
+    $compiler->setOutputStyle($style);
+
+    // write CSS to file
+    $css = $compiler->compileString($content)->getCss();
+    $this->wire->files->filePutContents($output, $css);
+  }
+
+  /**
+   * Compile input file to output file if any watched file is newer than output
+   *
+   *
+   * Returns TRUE if file has been compiled
+   * and FALSE if nothing was done
+   */
+  public function compileIfChanged(
+    string $input,
+    array $watch,
+    string $output = null,
+    string $style = 'compressed',
+  ): bool {
+    if (!is_file($input)) throw new WireException("$input not found");
+
+    // auto-create output path if no custom one is set
+    if (!$output) $output = substr($input, 0, -4) . "css";
+
+    // get modified timestamp of output file
+    $mCSS = 0;
+    if (is_file($output)) $mCSS = filemtime($output);
+
+    // loop all watched files
+    // as soon if we find one that is newer than the output we compile new
+    foreach ($watch as $file) {
+      if (filemtime($file) <= $mCSS) continue;
+      $this->compile($input, $output, $style);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Get the compiler instance
+   */
+  public function compiler(): Compiler
   {
+    if ($this->compiler) return $this->compiler;
     require_once __DIR__ . "/vendor/autoload.php";
     $this->compiler = new Compiler();
+    return $this->compiler;
   }
 
   /**
@@ -41,7 +105,7 @@ class Scss extends WireData implements Module
   {
     $path = $this->wire->config->paths->wire;
     $opt = ['extensions' => ['scss']];
-    $this->compiler->setOutputStyle(OutputStyle::COMPRESSED);
+    $this->compiler()->setOutputStyle(OutputStyle::COMPRESSED);
     foreach ($this->wire->files->find($path, $opt) as $scss) {
       $css = substr($scss, 0, -4) . "css";
       if (!is_file($css)) continue;
@@ -49,7 +113,7 @@ class Scss extends WireData implements Module
       $content = $this->wire->files->fileGetContents($scss);
       $this->wire->files->filePutContents(
         $css,
-        $this->compiler->compileString($content)->getCss()
+        $this->compiler()->compileString($content)->getCss()
       );
     }
   }
@@ -57,7 +121,7 @@ class Scss extends WireData implements Module
   public function __debugInfo()
   {
     return [
-      'compiler' => $this->compiler,
+      'compiler' => $this->compiler(),
     ];
   }
 }
